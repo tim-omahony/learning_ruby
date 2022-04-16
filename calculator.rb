7887c1b8eeafc507b93ad223cff8506ab4bfd24b
Tim <timothyomahony@gmail.com>
2022-04-16 15:31:40 +0100

    initial commit

---
+++ calculator.rb
@@ -0,0 +1,5 @@
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+puts "Enter another number: "
+num2 = gets.chomp().to_f
+puts "The sum of #{num1} and #{num2} is #{num1 + num2}"
---
+++ vars.rb
@@ -0,0 +1,3 @@
+character_name = "John"
+character_age = 35
+puts "There once was a man named #{character_name} who was #{character_age} years old."
